6295f0eef7edc62e5b6b5afe94f6fc9a2d8ecf9f
Nosheen Adil <nosheen.0398@gmail.com>
2020-05-29 14:09:07 -0400

    Lab 1

---
+++ lab1/Lab1_HeadA/translations/translation_en_US.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline/behavior_layer1/keyframe1/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Look forward</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Look forward</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline/behavior_layer1/keyframe1/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Look right</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Look right</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline/behavior_layer1/keyframe1/Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Look left</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Look left</translation>
+        </message>
+    </context>
+</TS>
